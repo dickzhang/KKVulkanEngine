40679ee0c12d9347a0a2fd009cdcca138cfb199a
KK <145623605+dickzhang@users.noreply.github.com>
2024-03-30 15:49:41 +0800

    添加动态缓存更新的示例

--- external/assimp/revision.h
+++ external/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x5248dd08
+#define GitVersion 0x50e673ae
 #define GitBranch "main"
 
 #endif // ASSIMP_REVISION_H_INC
