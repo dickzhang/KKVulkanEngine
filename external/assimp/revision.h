9565ed2554026b7ef233026483d5a8e0ff59c207
KK <145623605+dickzhang@users.noreply.github.com>
2024-04-20 22:12:05 +0800

    添加光线追踪示例

--- external/assimp/revision.h
+++ external/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0xd76080d2
+#define GitVersion 0x6b5f5702
 #define GitBranch "main"
 
 #endif // ASSIMP_REVISION_H_INC
