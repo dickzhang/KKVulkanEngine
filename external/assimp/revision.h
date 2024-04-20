d76080d2f350988f24cdee71f2cbd13ecf4cde53
KK <145623605+dickzhang@users.noreply.github.com>
2024-04-20 21:56:41 +0800

    添加硬件的光线追踪反射

--- external/assimp/revision.h
+++ external/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0xf9e69f7f
+#define GitVersion 0x87020f93
 #define GitBranch "main"
 
 #endif // ASSIMP_REVISION_H_INC
